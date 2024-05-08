d2323ed14cb85deec2924762c5bf9b1059804997
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-05-08 21:29:02 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,18 @@
+
+/**
+ * @param {number[]} candidates
+ * @return {number}
+ */
+var largestCombination = function (candidates) {
+    const BITS_IN_INT32 = 32;
+    let largestCombination = 0;
+
+    for (let shifts = 0; shifts < BITS_IN_INT32; ++shifts) {
+        let frequencyCurrentBit = 0;
+        for (let value of candidates) {
+            frequencyCurrentBit += (value >> shifts) & 1;
+        }
+        largestCombination = Math.max(largestCombination, frequencyCurrentBit);
+    }
+    return largestCombination;
+};
